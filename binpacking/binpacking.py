bea9c6d39345286c27c86413c8a85be69243db3e
RMesado <rafaelmesado@gmail.com>
2020-10-28 12:36:23 +0100

    binpacking. Corregir el 3ero

---
+++ binpacking/binpacking.py
@@ -0,0 +1,60 @@
+from typing import *
+from random import random, seed
+
+
+def mientras_quepa(W: List[int], C: int) -> List[int]:
+    sol = []
+    espacio_libre = C
+    ultimo_contenedor = 0
+    for w in W:
+        if w > espacio_libre:
+            espacio_libre = C
+            ultimo_contenedor += 1
+        espacio_libre -= w
+        sol.append(ultimo_contenedor)
+    return sol
+
+
+def primero_que_quepa(W: List[int], C: int) -> List[int]:
+    sol = []
+    contenedores = [C] * len(W)
+    for w in W:
+        for pos in range(len(contenedores)):
+            if w <= contenedores[pos]:
+                contenedores[pos] -= w
+                sol.append(pos)
+                break
+    return sol
+
+
+def primero_que_quepa_ordenado(W: List[int], C: int) -> List[int]:
+    sol = []
+    contenedores = [C] * len(W)
+    objetos = W.copy()
+    objetos.sort(reverse=True)
+    for w in objetos:
+        for pos in range(len(contenedores)):
+            if w <= contenedores[pos]:
+                contenedores[pos] -= w
+                sol.append(pos)
+                break
+    return sol
+
+
+def prueba_binpacking():
+    W, C = [1, 2, 8, 7, 8, 3], 10
+    # seed(42)
+    # W, C = [int(random()*1000)+1 for i in range(1000)], 1000
+
+    for solve in [mientras_quepa, primero_que_quepa, primero_que_quepa_ordenado]:
+        print("-" * 40)
+        print("Método:", solve.__name__)
+        try:
+            sol = solve(W, C)
+            print("Usados {} contenedores: {}".format(1 + max(sol), sol))
+        except NotImplementedError:
+            print("No implementado")
+
+
+if __name__ == "__main__":
+    prueba_binpacking()
