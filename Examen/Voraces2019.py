e62d0741b19dd453f5f36667f96530336390e217
RMesado <rafaelmesado@gmail.com>
2021-04-13 16:46:57 +0200

    Voraces bien hecho

--- Examen/Voraces2019.py
+++ Examen/Voraces2019.py
@@ -2,23 +2,28 @@ from typing import *
 
 
 def ej1(M: int, L: List[int], B: List[int], P: List[int]):
-    sol = [0] * len(L)
-    m_restante = M
-    indices_ord_L = sorted(range(len(L)), key=lambda i: -B[i] / L[i])
+    sol = [0] * len(L)  # Paso 1: lista de solución vacía
+    beneficio = 0
+    peso_usado = 0
+    indices_ord_L = sorted(range(len(L)),
+                           key=lambda i: -B[i] / P[i])  # Paso 2: ordenar una lista de indices de ratio Beneficio/Peso
     for i in indices_ord_L:
-        for pos in range(len(L)):
-            l_act = L[i]
-            p_act = P[i]
-            while p_act > 0:
-                espacio_gastado = l_act * p_act
-                if espacio_gastado <= m_restante:
-                    m_restante -= espacio_gastado
-                    sol[i] = p_act
-                    break
-                p_act -= 1
-            break
+        l_act = L[i]
+        p_act = P[i]
+        # while p_act > 0:  # --------NO USAR BUCLE WHILE PEDAZO DE SUBNORMAL
+        #   espacio_gastado = l_act * p_act
+        #  if espacio_gastado <= M:
+        #     M -= espacio_gastado
+        #    sol[i] = p_act
+        #   break
+        # p_act -= 1
+        cogemos = min(M // P[i], L[i])
+        sol[i] = cogemos
+        M -= cogemos * P[i]
+        peso_usado += cogemos * P[i]
+        beneficio += cogemos * B[i]
 
-    return sol
+    return beneficio, sol, peso_usado
 
 
 def beneficio(v: List[int], sol: List[int]) -> int:
@@ -26,19 +31,20 @@ def beneficio(v: List[int], sol: List[int]) -> int:
 
 
 if __name__ == "__main__":
-    M = 94
+    M = 50
     L = [2, 9, 5, 7, 6, 1, 4]
     P = [2, 3, 1, 6, 1, 2, 5]
     B = [3, 5, 10, 9, 4, 8, 1]
     sol = ej1(M, L, B, P)
-    print(L)
-    print(P)
-    print(sol, beneficio(B, sol))
+    print('Cantidad Obj.: ', L)
+    print('Pesos: ', P)
+    print('Beneficio: ', B)
+    print('Solucion(Beneficio, lista_sol, peso usado) y Beneficio calculado a parte: ', sol, beneficio(B, sol[1]))
     peso = 0
     ratio = []
     for i in range(len(L)):
-        ratio.append(B[i] / L[i])
-    print(ratio)
-    for i in range(len(sol)):
-        peso += sol[i] * L[i]
-    print(peso)
+        ratio.append(B[i] / P[i])
+    print('Ratio: ', ratio)
+    for i in range(len(sol[1])):
+        peso += sol[1][i] * P[i]
+    print('Peso usado: ', peso)
